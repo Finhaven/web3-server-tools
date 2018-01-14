35b748a17172ce974918d015937e41a4a313c9d9
User <naumenko.gs@gmail.com>
2018-01-14 15:30:29 -0800

    macos specific fixes

--- ensure-ganache-running.sh
+++ ensure-ganache-running.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-PROCESS_COUNT=`pgrep -af ganache-cli | wc -l`
+PROCESS_COUNT=`ps aux | grep ganache-cli | wc -l`
 if [ "$PROCESS_COUNT" -lt "2" ];  #one process will be pgrep, if two processes then ganache-cli is running
 then
     echo "ganache-cli is not running.  you can launch with: npm run start:testrpc &"
