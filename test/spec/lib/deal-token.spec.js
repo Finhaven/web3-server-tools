86e78c3a1689e5ffeb7fe39ec521f7aee2ee2150
User <naumenko.gs@gmail.com>
2018-01-26 13:52:22 -0800

    wip update

--- test/spec/lib/deal-token.spec.js
+++ test/spec/lib/deal-token.spec.js
@@ -12,9 +12,14 @@ describe('DealToken', () => {
     privateKey: keys[0], // '0xb3bbe022606f7b7345df92ab110d1582566a7be7487e34f5085dd49cb5236369',
   };
 
+  const options = {
+    params: [],
+    txParams: {from: testAccount.address, gas: '6712388', gasPrice: '0x174876e800'},
+  };
+
   let dealToken;
 
-  async function checkBalance(account, expected) {
+  function checkBalance(account, expected) {
     return dealToken.methods.balanceOf(account).call()
       .then((balance) => {
         console.log('balance of token', balance);
@@ -23,24 +28,30 @@ describe('DealToken', () => {
   }
 
 
-  async function mintTokens(account, amountToMint) {
-    return dealToken.methods.mint(account, amountToMint).call()
-      .then(() => dealToken.methods.approve(account, amountToMint).call());
+  function mintTokens(account, amountToMint) {
+    return dealToken.methods.mint(account, amountToMint).send(options.txParams)
+      .then(() => dealToken.methods.approve(account, amountToMint).call(options.txParams));
   }
 
 
-  async function authorize(account) {
-    return () => dealToken.methods.authorizeAddress(account, true).call();
+  function authorize(account) {
+    console.log('authorizing');
+    return dealToken.methods.authorizeAddress(account, true).send(options.txParams);
   }
 
-  async function checkAuthorized(account, expected) {
-    return () => dealToken.methods.isAuthorized(account).call()
+  function unauthorize(account) {
+    console.log('authorizing');
+    return dealToken.methods.authorizeAddress(account, false).send(options.txParams);
+  }
+
+  function checkAuthorized(account, expected) {
+    console.log('checking auth');
+    return dealToken.methods.isAuthorized(account).call()
       .then((authorized) => assert.equal(expected, authorized));
   }
 
-  async function transfer(from, to, amount, shouldFail) {
-    return dealToken.methods
-      .transferFrom(from, to, amount).call()
+  function transfer(from, to, amount, shouldFail) {
+    return dealToken.methods.transferFrom(from, to, amount).send(options.txParams)
       .then((result) => {
         if (shouldFail) {
           console.log('transfer success - should happen now', result);
@@ -49,10 +60,10 @@ describe('DealToken', () => {
           assert.fail('transfer succeeded', 'transfer should fail');
         }
       })
-      .catch(() => {
+      .catch((e) => {
         // if transfer should fail this is ok
         if (!shouldFail) {
-          // console.log('transfer failed', e);
+          console.log('transfer failed', e);
           assert.fail('transfer failed', 'transfer should succeed');
         }
       });
@@ -60,15 +71,13 @@ describe('DealToken', () => {
 
   const ownerAccount = accounts[0];
   const userAccount = accounts[1];
-  const userAccount2 = accounts[2];
 
-  beforeEach(async () => {
+  beforeEach(() => {
     console.log('redeploying token');
-    const options = {
-      params: [],
-      txParams: {from: testAccount.address, gas: '6712388', gasPrice: '0x174876e800'},
-    };
-    dealToken = await Eth.deployContract('DealToken', options);
+    return Eth.deployContract('DealToken', options)
+    .then(result => {
+      dealToken = result;
+    })
   });
 
   it('should get instance of lp token', () => {
@@ -77,61 +86,53 @@ describe('DealToken', () => {
 
   it('should get zero balance of lp token', () => checkBalance(userAccount, 0));
 
-  it('should authorize ', async () => {
-      return checkAuthorized(userAccount, false)
-        .then(() => authorize(userAccount))
-        .then(() => checkAuthorized(userAccount, false));
-  });
-
-  it('should authorize ', async () => {
-      return checkAuthorized(userAccount, false)
-        .then(() => authorize(userAccount))
-        .then(() => checkAuthorized(userAccount, true));
-  });
-
-
-  // it('should mint tokens ', async () => {
-  //   return Promise.resolve()
-  //     .then(authorize(userAccount))
-  //     .then(checkBalance(userAccount, 0))
-  //     .then(mintTokens(userAccount, 25))
-  //     .then(checkBalance(userAccount, 25));
+  // it('should authorize ', () => {
+  //     return checkAuthorized(userAccount, false)
+  //       .then(() => authorize(userAccount))
+  //       .then(() => checkAuthorized(userAccount, true));
   // });
 
-  // it('should transfer tokens to authorized address ', async () => {
-  //   return Promise.resolve()
-  //     .then(checkBalance(userAccount, 0))
-  //     .then(authorize(userAccount))
-  //     .then(authorize(userAccount2))
-  //     .then(mintTokens(ownerAccount, 100))
-  //     .then(checkBalance(ownerAccount, 100))
-  //     .then(transfer(ownerAccount, userAccount, 33, false))
-  //     .then(checkBalance(userAccount, 33))
-  //     .then(checkBalance(ownerAccount, 67));
+  // it('should mint tokens ', () => {
+  //   return authorize(userAccount)
+  //     .then(() => checkBalance(userAccount, 0))
+  //     .then(() => mintTokens(userAccount, 25))
+  //     .then(() => checkBalance(userAccount, 25));
+  // });
+
+  // it('should transfer tokens to authorized address ', () => {
+  //     const shouldFail = false;
+  //     return checkBalance(ownerAccount, 0)
+  //     .then(() => authorize(ownerAccount))
+  //     .then(() => authorize(userAccount))
+  //     .then(() => mintTokens(ownerAccount, 100))
+  //     .then(() => checkBalance(ownerAccount, 100))
+  //     .then(() => transfer(ownerAccount, userAccount, 33, shouldFail));
+  //     // .then(() => checkBalance(userAccount, 33))
+  //     // .then(() => checkBalance(ownerAccount, 67));
   // });
 
 
   // it('should not transfer tokens to an unauthorized address', () => {
   //   const shouldFail = true;
-  //   return Promise.resolve()
-  //     .then(checkBalance(userAccount, 0))
-  //     .then(authorize(userAccount))
-  //     .then(mintTokens(userAccount, 100))
-  //     .then(transfer(userAccount, userAccount2, 40, shouldFail))
-  //     .then(checkBalance(userAccount2, 0))
-  //     .then(checkBalance(userAccount, 100));
+  //   return checkBalance(ownerAccount, 0)
+  //     .then(() => authorize(ownerAccount))
+  //     .then(() => mintTokens(ownerAccount, 100))
+  //     .then(() => transfer(ownerAccount, userAccount, 40, shouldFail))
+  //     .then(() => checkBalance(userAccount, 0))
+  //     .then(() => checkBalance(ownerAccount, 100));
   // });
 
-  // it('should not transfer tokens from an unauthorized address', () => {
-  //   const shouldFail = true;
-  //   return Promise.resolve()
-  //     .then(checkBalance(userAccount, 0))
-  //     .then(authorize(userAccount2))
-  //     .then(mintTokens(userAccount, 100))
-  //     .then(transfer(userAccount, userAccount2, 40, shouldFail))
-  //     .then(checkBalance(userAccount2, 0))
-  //     .then(checkBalance(userAccount, 100));
-  // });
+  it('should not transfer tokens from an unauthorized address', () => {
+    const shouldFail = true;
+      return checkBalance(ownerAccount, 0)
+      .then(() => authorize(userAccount))
+      .then(() => authorize(ownerAccount))
+      .then(() => mintTokens(ownerAccount, 100))
+      .then(() => unauthorize(ownerAccount))
+      .then(() => transfer(ownerAccount, userAccount, 40, shouldFail))
+      .then(() => checkBalance(userAccount, 0))
+      .then(() => checkBalance(ownerAccount, 100));
+  });
 
 });
 
