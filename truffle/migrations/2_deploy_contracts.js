c0bd7b71e7c87b76c49bbe3d8b839e0cd9c84cd2
Brooklyn Zelenka <be.zelenka@gmail.com>
2018-03-23 20:43:30 -0700

    Remove unneeded contract

--- truffle/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const Deal = artifacts.require('./Deal.sol');
-
-module.exports = (deployer) => {
-  // let gasLimit = web3.eth.getBlock('pending').gasLimit;
-  // console.log('block gasLimit',gasLimit);
-  deployer.deploy(Deal);
-};
